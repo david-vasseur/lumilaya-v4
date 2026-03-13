15752e05ef998b029f6f7022eda4947dd5cf5eda
David <p5y4@laposte.net>
2026-03-13 16:08:13 +0100

    test

--- components/features/Nav.tsx
+++ components/features/Nav.tsx
@@ -25,7 +25,7 @@ function Nav() {
                         {[
                             { label: "Accueil", href: "/" },
                             { label: "Notre histoire", href: "/notre-histoire" },
-                            { label: "Bougies Emotions & Plaisirs", href: "/bougies-emotions" },
+                            { label: "Bougies Emotions & Plaisirs", href: "/bougies-emotion" },
                             { label: "Bougies Entre Terre & Ciel", href: "/bougies-rituel" },
                             { label: "Mes favoris", href: "/favoris" },
                             { label: "Nous contacter", href: "/contact" },
