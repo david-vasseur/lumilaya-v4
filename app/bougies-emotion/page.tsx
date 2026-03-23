e463da3c69052c21a288582157d460acdc0ed675
David <p5y4@laposte.net>
2026-03-23 14:17:57 +0100

    bdd push

--- app/bougies-emotion/page.tsx
+++ app/bougies-emotion/page.tsx
@@ -3,56 +3,62 @@ import Hero from '@/components/layout/product_list_page/Hero';
 import Interlude from '@/components/layout/product_list_page/Interlude';
 import ProductsList from '@/components/layout/product_list_page/ProductsList';
 import { ProductCardProps } from '@/components/ui/ProductCard';
+import { getProductsByCollection } from '@/lib/action/product.action';
 
-export const mockProducts: ProductCardProps[] = [
-    {
-        slug: 'bougie-magie',
-        collection: "bougies-emotion",
-        image: '/images/products/magie.webp',
-        name: 'Bougie Magie',
-        duration: '12',
-        intro: 'Une bougie envoûtante qui illumine vos soirées et éveille vos sens.',
-        price: 19.90
-    },
-    {
-        slug: 'bougie-tendresse',
-        collection: "bougies-emotion",
-        image: '/images/products/tendresse.webp',
-        name: 'Bougie Tendresse',
-        duration: '10',
-        intro: 'Douceur et réconfort dans un parfum délicat et apaisant.',
-        price: 19.90
-    },
-    {
-        slug: 'bougie-douceur',
-        collection: "bougies-emotion",
-        image: '/images/products/douceur.webp',
-        name: 'Bougie Douceur',
-        duration: '8',
-        intro: 'Un parfum léger et enveloppant pour des moments de calme.',
-        price: 19.90
-    },
-    {
-        slug: 'bougie-vitalite',
-        collection: "bougies-emotion",
-        image: '/images/products/vitalite.webp',
-        name: 'Bougie Vitalité',
-        duration: '14',
-        intro: 'Énergisante et rafraîchissante, elle dynamise votre espace.',
-        price: 19.90
-    },
-    {
-        slug: 'bougie-harmonie',
-        collection: "bougies-emotion",
-        image: '/images/products/harmonie.webp',
-        name: 'Bougie Harmonie',
-        duration: '12',
-        intro: 'Pour retrouver équilibre et sérénité dans votre quotidien.',
-        price: 19.90
-    }
-];
+// export const mockProducts: ProductCardProps[] = [
+//     {
+//         slug: 'bougie-magie',
+//         collection: "bougies-emotion",
+//         image: '/images/products/magie.webp',
+//         name: 'Bougie Magie',
+//         duration: '12',
+//         intro: 'Une bougie envoûtante qui illumine vos soirées et éveille vos sens.',
+//         price: 19.90
+//     },
+//     {
+//         slug: 'bougie-tendresse',
+//         collection: "bougies-emotion",
+//         image: '/images/products/tendresse.webp',
+//         name: 'Bougie Tendresse',
+//         duration: '10',
+//         intro: 'Douceur et réconfort dans un parfum délicat et apaisant.',
+//         price: 19.90
+//     },
+//     {
+//         slug: 'bougie-douceur',
+//         collection: "bougies-emotion",
+//         image: '/images/products/douceur.webp',
+//         name: 'Bougie Douceur',
+//         duration: '8',
+//         intro: 'Un parfum léger et enveloppant pour des moments de calme.',
+//         price: 19.90
+//     },
+//     {
+//         slug: 'bougie-vitalite',
+//         collection: "bougies-emotion",
+//         image: '/images/products/vitalite.webp',
+//         name: 'Bougie Vitalité',
+//         duration: '14',
+//         intro: 'Énergisante et rafraîchissante, elle dynamise votre espace.',
+//         price: 19.90
+//     },
+//     {
+//         slug: 'bougie-harmonie',
+//         collection: "bougies-emotion",
+//         image: '/images/products/harmonie.webp',
+//         name: 'Bougie Harmonie',
+//         duration: '12',
+//         intro: 'Pour retrouver équilibre et sérénité dans votre quotidien.',
+//         price: 19.90
+//     }
+// ];
 
-function page() {
+async function page() {
+
+    const products = await getProductsByCollection('Emotion');
+
+    if (!products) return;
+
     return (
         <div className="min-h-screen bg-[#FDFBF7]">
             <Hero 
@@ -62,7 +68,7 @@ function page() {
                 subtitle={"Des bougies parfumées qui éveillent les sens et enveloppent le quotidien de douceur. <br/>Une invitation à créer une atmosphère chaleureuse et à savourer des instants de réconfort et de plaisir."} 
             />
             <Interlude collection='Emotion' />
-            <ProductsList products={mockProducts} />
+            <ProductsList products={products} />
             <Final />
         </div>
     )
--- lib/action/product.action.ts
+++ lib/action/product.action.ts
@@ -6,43 +6,44 @@ import { products } from "@/data/product";
 
 /////// ACTION POUR RECUPERER TOUS LES PRODUITS D'UNE COLLECTION //////////////////
 
-// export const getProductsByCollection = async (collection: string) => {
+export const getProductsByCollection = async (collection: string) => {
 	
-// 	const products: Array<{
-// 		images: string[]
-// 		variants: { price: number; duration: number }[]
-// 		meta: { slug: string; collection: string; name: string; intro: string }
-// 	}> = await prisma.product.findMany({
-// 		where: {
-// 			meta: {
-// 				collection: collection
-// 			}
-// 		},
-// 		select: { 
-// 			images: true,
-// 			variants: {
-// 				select: { price: true, duration: true },
-// 				orderBy: { price: 'asc' },
-// 				take: 1
-// 			},
-// 			meta: {
-// 				select: { slug: true, collection: true, name: true, intro: true }
-// 			}
-// 		}
-// 	})
+	const products = await prisma.product.findMany({
+		where: {
+			meta: {
+				collection: collection
+			}
+		},
+		select: { 
+			images: true,
+			variants: {
+				select: { price: true, duration: true },
+				orderBy: { price: 'asc' },
+				take: 1
+			},
+			meta: {
+				select: { slug: true, collection: true, name: true, intro: true }
+			}
+		}
+	})
 
-// 	const cards: ProductCardProps[] = products.map(p => ({
-// 		slug: p.meta.slug,
-// 		collection: p.meta.collection,
-// 		image: p.images[0],                   
-// 		name: p.meta.name,
-// 		intro: p.meta.intro,
-// 		duration: `${p.variants[0]?.duration}`,
-// 		price: p.variants[0]?.price ?? 0
-// 	}))
+	const cards = products.map(p => ({
+		slug: p.meta.slug,
+		collection: p.meta.collection,
+		image: p.images[0],                   
+		name: p.meta.name,
+		intro: p.meta.intro,
+		duration: `${p.variants[0]?.duration}`,
+		price: Number(p.variants[0]?.price) ?? 0
+	}))
 
-// 	return cards;
-// }
+	return cards;
+}
+
+/////// ACTION POUR RECUPERER LES PRODUITS D'UNE COLLECTION //////////////////
+
+
+
 
 /////// ACTION POUR RECUPERER UN PRODUIT //////////////////
 
