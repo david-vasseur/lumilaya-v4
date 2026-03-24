ffba75952bc4d89e3cec19f8a889d9cd20b69e91
David <p5y4@laposte.net>
2026-03-24 08:20:17 +0100

    test suggested products

--- app/bougies-rituel/[slug]/page.tsx
+++ app/bougies-rituel/[slug]/page.tsx
@@ -1,7 +1,8 @@
 import ProductCar from '@/components/features/product/ProductCar';
 import ProductConseil from '@/components/features/product/ProductConseil';
 import Principal from '@/components/layout/product_page/Principal';
-import { getOneProductBySlug } from '@/lib/action/product.action';
+import Suggest from '@/components/layout/product_page/Suggest';
+import { getOneProductBySlug, getSuggestedProduct } from '@/lib/action/product.action';
 
 interface Props {
     params: { slug: string };
@@ -15,15 +16,19 @@ async function page({ params }: Props) {
 
     // const product = products.find((product) => product.meta.slug === slug)!
     const product = await getOneProductBySlug(slug);
-    console.log(product);
     
-    if (!product) return
+    if (!product) return;
+
+    const suggestedProducts = await getSuggestedProduct(product.id);
+    
+    console.log(suggestedProducts);
 
     return (
         <div className="min-h-screen bg-[#FDFBF7] pt-24 pb-20 max-w-7xl mx-auto px-6">
             <Principal product={product} />
             <ProductCar />
             <ProductConseil />
+            <Suggest suggestedProducts={suggestedProducts} />
         </div>
         
     )
