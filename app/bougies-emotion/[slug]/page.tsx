e73f7a9b1987df3fe2b0fa8acd260024b8583d27
David <p5y4@laposte.net>
2026-03-26 16:36:37 +0100

    adding SEO on products pages

--- app/bougies-emotion/[slug]/page.tsx
+++ app/bougies-emotion/[slug]/page.tsx
@@ -33,7 +33,7 @@ export async function generateMetadata({ params }: Props) {
             siteName: "Lumilaya",
             images: [
                 {
-                    url: product.images,
+                    url: product.images?.[0] || "",
                     width: 1200,
                     height: 630,
                     alt: product.meta.name,
@@ -70,7 +70,7 @@ async function page({ params }: Props) {
         "@type": "Product",
         name: product.meta.name,
         description: product.meta.intro,
-        image: product.images[0],
+        image: product.images?.[0] || "",
         sku: product.id,
         brand: {
             "@type": "Brand",
