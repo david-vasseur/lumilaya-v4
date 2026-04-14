6b84e1e3c33455d9de0871dcdb75e1dff6127704
David <p5y4@laposte.net>
2026-04-14 07:43:48 +0200

    new section decouverte

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import BestProducts from "@/components/layout/landing/BestProducts";
 import Collections from "@/components/layout/landing/Collections";
-import CoffretDecouverte from "@/components/layout/landing/Decouverte";
 import Event from "@/components/layout/landing/Event";
 import Faq from "@/components/layout/landing/Faq";
 import Hero from "@/components/layout/landing/Hero";
@@ -13,7 +12,6 @@ export default async function Home() {
     return (
 		<>
 			<Hero />
-			<CoffretDecouverte />
 			<Quality />
 			<Collections />
 			<Event />
