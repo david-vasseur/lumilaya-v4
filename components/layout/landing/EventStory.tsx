0bbc0bd8d3e8d93453cb1f994e26e4c5186b42ea
David <p5y4@laposte.net>
2026-07-26 11:40:55 +0200

    new hero

--- components/layout/landing/EventStory.tsx
+++ components/layout/landing/EventStory.tsx
@@ -1,48 +1,374 @@
+// "use client"
+
+// import { useGSAP } from '@gsap/react'
+// import gsap from 'gsap'
+
+// function EventStory() {
+
+//     useGSAP(() => {
+
+//         gsap.set('.event-image', {
+//             opacity: 1,
+//             WebkitMaskSize: '0% 0%',
+//             maskSize: '0% 0%',
+//         })
+
+//         const tl = gsap.timeline({
+//             scrollTrigger: {
+//                 trigger: '.events-section',
+//                 start: 'top top',
+//                 end: '+=100%',
+//                 scrub: 1,
+//                 pin: true,
+//                 pinSpacing: false
+//             }
+//         })
+
+//         // 1. Les images apparaissent
+//         tl.to('.event-image', {
+//             WebkitMaskSize: '200% 200%',
+//             maskSize: '200% 200%',
+//             duration: 0.5,
+//             ease: 'power2.out',
+//         }, 0.05)
+
+//         // 2. Puis les rideaux s'ouvrent
+//         tl.to('.curtain-left', {
+//             xPercent: -100,
+//         }, '+=0.05')
+
+//         tl.to('.curtain-right', {
+//             xPercent: 100,
+//         }, '<')
+
+//     })
+
+
+//     return (
+//         <section className="
+//             events-section
+//             relative
+//             w-full
+//             h-lvh
+//             overflow-hidden
+//             z-20
+//         ">
+
+//             {/* CONTENEUR GAUCHE */}
+//             <div
+//                 className="
+//                     curtain-left
+//                     absolute
+//                     left-0
+//                     top-0
+
+//                     w-full
+//                     h-full
+
+//                     bg-[#5A7B6E]
+//                     overflow-hidden
+//                     z-20
+//                 "
+//                 style={{
+//                         clipPath: 'polygon(0 0, 50.1% 0, 50.1% 100%, 0 100%)'
+//                     }}
+//             >
+//                 <picture
+//                     className="
+//                         event-image
+//                         absolute
+//                         top-1/2
+//                         left-1/2
+//                         opacity-0
+//                         w-full
+//                         h-full
+//                         z-20
+//                         -translate-x-1/2
+//                         -translate-y-1/2
+
+//                         md:w-[80vw]
+//                         md:max-w-5xl
+//                         md:h-auto
+//                         md:aspect-16/10
+//                         md:mx-auto
+//                     "
+//                     style={{
+//                         WebkitMaskImage: "url('/images/landing/noise.png')",
+//                         maskImage: "url('/images/landing/noise.png')",
+
+//                         WebkitMaskSize: '200% 200%',
+//                         maskSize: '200% 200%',
+
+//                         WebkitMaskPosition: 'center',
+//                         maskPosition: 'center',
+
+//                         WebkitMaskRepeat: 'no-repeat',
+//                         maskRepeat: 'no-repeat',
+//                     }}
+//                 >
+//                     <img
+//                         src="/images/landing/marche-desk.webp"
+//                         className="w-full h-full object-cover"
+//                         alt=""
+//                     />
+//                 </picture>
+//                 <h2 className="
+//                     absolute z-15
+//                     inset-0
+//                     flex
+//                     items-center
+//                     justify-center
+//                     text-center
+//                     font-semibold
+//                     text-white/90
+//                     text-7xl
+//                 ">
+//                     Venez nous rencontrer
+//                 </h2>
+//             </div>
+
+
+//             {/* CONTENEUR DROIT */}
+//             <div
+//                 className="
+//                     curtain-right
+//                     absolute
+//                     right-0
+//                     top-0
+                                                
+//                     w-full
+//                     h-full
+
+//                     bg-[#5A7B6E]
+//                     overflow-hidden
+//                     z-20
+//                 "
+//                 style={{
+//                         clipPath: 'polygon(50% 0, 100% 0, 100% 100%, 50% 100%)'
+//                     }}
+//             >
+//                 <picture
+//                     className="
+//                         event-image
+//                         absolute
+//                         top-1/2
+//                         left-1/2
+//                         opacity-0
+//                         w-full
+//                         h-full
+//                         z-20
+//                         -translate-x-1/2
+//                         -translate-y-1/2
+
+//                         md:w-[80vw]
+//                         md:max-w-5xl
+//                         md:h-auto
+//                         md:aspect-16/10
+//                         md:mx-auto
+//                     "
+//                     style={{
+//                         WebkitMaskImage: "url('/images/landing/noise.png')",
+//                         maskImage: "url('/images/landing/noise.png')",
+
+//                         WebkitMaskSize: '200% 200%',
+//                         maskSize: '200% 200%',
+
+//                         WebkitMaskPosition: 'center',
+//                         maskPosition: 'center',
+
+//                         WebkitMaskRepeat: 'no-repeat',
+//                         maskRepeat: 'no-repeat',
+//                     }}
+//                 >
+//                     <img
+//                         src="/images/landing/marche-desk.webp"
+//                         className="w-full h-full object-cover"
+//                         alt=""
+//                     />
+//                 </picture>
+//                 <h2 className="
+//                     absolute
+//                     inset-0
+//                     flex
+//                     items-center
+//                     justify-center
+//                     text-center
+//                     font-semibold
+//                     text-white/90
+//                     text-7xl
+//                     z-15
+//                 ">
+//                     Venez nous rencontrer
+//                 </h2>
+//             </div>
+
+//         </section>
+//     )
+// }
+
+// export default EventStory;
+
 "use client"
 
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
+import { useEffect, useRef, useState } from 'react'
+
+function RevealGrid() {
+
+    const gridRef = useRef<HTMLDivElement | null>(null);
+
+    const [grid, setGrid] = useState({
+        columns: 50,
+        rows: 30,
+    })
+
+    const cellSize = 20
+
+    useEffect(() => {
+
+         const element = gridRef.current
+
+        if (!element) return
+
+        const updateGrid = () => {
+
+            const {
+                width,
+                height
+            } = element.getBoundingClientRect()
+
+            
+
+            const columns =
+                Math.ceil(width / cellSize)
+
+            const rows =
+                Math.ceil(height / cellSize)
+
+            setGrid({
+                columns,
+                rows,
+            })
+        }
+
+        updateGrid()
+
+        const resizeObserver =
+            new ResizeObserver(updateGrid)
+
+        resizeObserver.observe(
+            element
+        )
+
+        return () => {
+            resizeObserver.disconnect()
+        }
+
+    }, [])
+
+    const totalCells =
+        grid.columns * grid.rows
+
+    return (
+        <div
+            ref={gridRef}
+            className={`
+                reveal-grid
+                absolute
+                inset-0
+                z-10
+                grid
+                -translate-x-px
+            `}
+            style={{
+                gridTemplateColumns:
+                    `repeat(${grid.columns}, 20px)`,
+
+                gridTemplateRows:
+                    `repeat(${grid.rows}, 20px)`,
+            }}
+        >
+            {Array.from({
+                length: totalCells
+            }).map((_, index) => (
+
+                <div
+                    key={index}
+                    className="
+                        reveal-cell
+                        bg-[#6F9083]
+                    "
+                    style={{
+                        width: `${cellSize + 1}px`,
+                        height: `${cellSize + 1}px`,
+                        marginRight: '-1px',
+                        marginBottom: '-1px',
+                    }}
+                />
+
+            ))}
+        </div>
+    )
+}
+
 
 function EventStory() {
 
     useGSAP(() => {
 
-        gsap.set('.event-image', {
-            opacity: 1,
-            WebkitMaskSize: '0% 0%',
-            maskSize: '0% 0%',
-        })
+        gsap.set(".image-reveal", { opacity: 0 })
 
         const tl = gsap.timeline({
             scrollTrigger: {
                 trigger: '.events-section',
                 start: 'top top',
-                end: '+=100%',
+                end: '+=200%',
                 scrub: 1,
                 pin: true,
                 pinSpacing: false
             }
         })
 
-        // 1. Les images apparaissent
-        tl.to('.event-image', {
-            WebkitMaskSize: '200% 200%',
-            maskSize: '200% 200%',
-            duration: 0.5,
+        // 1. La phrase disparaît
+        tl.to('.reveal-title', {
+            opacity: 0,
+            duration: 0.6,
             ease: 'power2.out',
-        }, 0.05)
+        }, 0)
 
-        // 2. Puis les rideaux s'ouvrent
+        // 2. La pixelisation révèle l'image
+        tl.to('.reveal-cell', {
+            opacity: 0,
+            stagger: {
+                amount: 1,
+                from: 'random',
+            },
+            duration: 0.05,
+            ease: 'none',
+        }, 0.1)
+
+        // 3. L'image apparaît
+        tl.to('.image-reveal', {
+            opacity: 1,
+            duration: 0.05,
+        }, "<")
+
+        // 4. Les rideaux s'ouvrent
         tl.to('.curtain-left', {
             xPercent: -100,
+            duration: 0.6,
         }, '+=0.05')
 
         tl.to('.curtain-right', {
             xPercent: 100,
+            duration: 0.6,
         }, '<')
 
     })
 
+
+    const gridCells = Array.from({ length: 1200 })
 
     return (
         <section className="
@@ -51,7 +377,6 @@ function EventStory() {
             w-full
             h-lvh
             overflow-hidden
-            z-20
         ">
 
             {/* CONTENEUR GAUCHE */}
@@ -65,24 +390,44 @@ function EventStory() {
                     w-full
                     h-full
 
-                    bg-[#5A7B6E]
+                    bg-[#6F9083]
                     overflow-hidden
-                    z-20
+                    z-10
                 "
                 style={{
-                        clipPath: 'polygon(0 0, 50.1% 0, 50.1% 100%, 0 100%)'
-                    }}
+                    clipPath: 'polygon(0 0, 50.1% 0, 50.1% 100%, 0 100%)'
+                }}
             >
+
+                <h2 className="
+                    reveal-title
+                    bg-transparent
+                    absolute
+                    inset-0
+                    z-20
+                    flex
+                    items-center
+                    justify-center
+                    text-center
+                    text-5xl
+                    font-semibold
+                    text-white
+                ">
+                    Au-delà de l'écran. <br/>
+                    Découvrez-nous en vrai.
+                </h2>
+
+                {/* IMAGE */}
                 <picture
                     className="
-                        event-image
+                    image-reveal
                         absolute
                         top-1/2
                         left-1/2
-                        opacity-0
+
                         w-full
                         h-full
-                        z-20
+
                         -translate-x-1/2
                         -translate-y-1/2
 
@@ -92,39 +437,18 @@ function EventStory() {
                         md:aspect-16/10
                         md:mx-auto
                     "
-                    style={{
-                        WebkitMaskImage: "url('/images/landing/noise.png')",
-                        maskImage: "url('/images/landing/noise.png')",
-
-                        WebkitMaskSize: '200% 200%',
-                        maskSize: '200% 200%',
-
-                        WebkitMaskPosition: 'center',
-                        maskPosition: 'center',
-
-                        WebkitMaskRepeat: 'no-repeat',
-                        maskRepeat: 'no-repeat',
-                    }}
                 >
                     <img
                         src="/images/landing/marche-desk.webp"
-                        className="w-full h-full object-cover"
+                        className="
+                            w-full
+                            h-full
+                            object-cover
+                        "
                         alt=""
                     />
+                    <RevealGrid />
                 </picture>
-                <h2 className="
-                    absolute z-15
-                    inset-0
-                    flex
-                    items-center
-                    justify-center
-                    text-center
-                    font-semibold
-                    text-white/90
-                    text-7xl
-                ">
-                    Venez nous rencontrer
-                </h2>
             </div>
 
 
@@ -135,28 +459,47 @@ function EventStory() {
                     absolute
                     right-0
                     top-0
-                                                
+
                     w-full
                     h-full
 
-                    bg-[#5A7B6E]
+                    bg-[#6F9083]
                     overflow-hidden
-                    z-20
+                    z-10
                 "
                 style={{
-                        clipPath: 'polygon(50% 0, 100% 0, 100% 100%, 50% 100%)'
-                    }}
+                    clipPath: 'polygon(50% 0, 100% 0, 100% 100%, 50% 100%)'
+                }}
             >
+
+                <h2 className="
+                    reveal-title
+                    absolute
+                    inset-0
+                    z-20
+                    flex
+                    items-center
+                    justify-center
+                    text-center
+                    text-5xl
+                    font-semibold
+                    text-white
+                ">
+                    Au-delà de l'écran. <br/>
+                    Découvrez-nous en vrai.
+                </h2>
+
+                {/* IMAGE */}
                 <picture
                     className="
-                        event-image
+                    image-reveal
                         absolute
                         top-1/2
                         left-1/2
-                        opacity-0
+
                         w-full
                         h-full
-                        z-20
+
                         -translate-x-1/2
                         -translate-y-1/2
 
@@ -166,40 +509,18 @@ function EventStory() {
                         md:aspect-16/10
                         md:mx-auto
                     "
-                    style={{
-                        WebkitMaskImage: "url('/images/landing/noise.png')",
-                        maskImage: "url('/images/landing/noise.png')",
-
-                        WebkitMaskSize: '200% 200%',
-                        maskSize: '200% 200%',
-
-                        WebkitMaskPosition: 'center',
-                        maskPosition: 'center',
-
-                        WebkitMaskRepeat: 'no-repeat',
-                        maskRepeat: 'no-repeat',
-                    }}
                 >
                     <img
                         src="/images/landing/marche-desk.webp"
-                        className="w-full h-full object-cover"
+                        className="
+                            w-full
+                            h-full
+                            object-cover
+                        "
                         alt=""
                     />
+                    <RevealGrid />
                 </picture>
-                <h2 className="
-                    absolute
-                    inset-0
-                    flex
-                    items-center
-                    justify-center
-                    text-center
-                    font-semibold
-                    text-white/90
-                    text-7xl
-                    z-15
-                ">
-                    Venez nous rencontrer
-                </h2>
             </div>
 
         </section>
